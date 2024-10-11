6da794d865cfc8016e7f70ce493d92cbd0cfe765
alaco04 <andrea.laco1234@gmail.com>
2024-10-12 01:50:23 +0200

    worked on code, didnt finished it

--- Source.c
+++ Source.c
@@ -5,10 +5,48 @@
 #define BUFFER_SIZE 1024
 
 int count_rows(char* filename);
+//void ispis(int n, char* filename);
+
+typedef struct {
+	char ime[50];
+	char prezime[50];
+	int bodovi;
+}Student;
 
 
 int main() {
-	printf("Hello world");
+	int n,i,max;
+	
+	n = count_rows("Studenti.txt");
+	Student* studenti = NULL;
+	studenti = (Student*)malloc(n * sizeof(Student));
+	//ispis(n, "Studenti.txt");
+
+	int i;
+	double rel;
+	printf("Unesite maksimalan broj bodova: ");
+	scanf("%d", &max);
+
+	FILE* fp = NULL;
+	fp = fopen("Studenti.txt", "r");
+	if (fp == NULL) {
+		printf("Error opening file\n");
+		return ERROR_OF;
+	}
+
+
+	for (i = 0;i < n;i++) {
+		rel = (double)studenti[i].bodovi / max;
+		fscanf(fp, "%s %s %d",studenti[i].ime, studenti[i].prezime, &studenti[i].bodovi);
+	}
+	fclose(fp);
+
+	for (i = 0;i < n;i++) {
+		rel = (double)studenti[i].bodovi / max;
+		printf("%s %s %d %f\n", studenti[i].ime, studenti[i].prezime, studenti[i].bodovi, rel * 100);
+	}
+
+	free(studenti);
 	return 0;
 }
 
@@ -21,10 +59,11 @@ int count_rows(char* filename) {
 		printf("Error opening file\n");
 		return ERROR_OF;
 	}
-	while (!foef(fp)) {
+	while (!feof(fp)) {
 		fgets(buffer, BUFFER_SIZE, fp);
 			++count;
 	 }
 	fclose(fp);
-	return 0;
+	return count;
 }
+
